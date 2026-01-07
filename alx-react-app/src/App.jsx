4353500b5fb87c60f37e14cd83813076b2c6248d
Allan <nabuleallan1@gmail.com>
2026-01-07 11:15:57 +0300

    added userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
