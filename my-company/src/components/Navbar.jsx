14a0f2900931735ea4d8a8b4bde4f1953ab714f5
Allan <nabuleallan1@gmail.com>
2026-01-18 19:28:11 +0300

    added navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import Services from '../Services';
 
 function NavBar() {
   return (
-    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc', display: 'flex', marginTop: '20px'}}>
+    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc', display: 'flex', marginTop: '20px', backgroundColor: '#f0f0f0', justifyContent: 'center' }}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
