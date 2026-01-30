150c0bc6ad375f4ffb54fc4e03b52ef4d827bca2
Allan <nabuleallan1@gmail.com>
2026-01-30 09:05:40 +0300

    added advanced search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com';
 const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+const advSwearchUrl = 'https://api.github.com/search/users?q=';
 
 /**
  * Generic function to fetch data from GitHub API
