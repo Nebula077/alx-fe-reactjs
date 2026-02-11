774f52733df2c2a307b56f6be3fef3b2f0a30782
Allan <nabuleallan1@gmail.com>
2026-02-11 10:59:03 +0300

    added tailwind

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import newPage from './components/newPage'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-500'>Recipe Sharing Platform</h1>
+        <p>This is a recipe sharing platform</p>
+      </div>
+      <newPage />
+    </>
+  )
+}
+
+export default App
---
+++ recipe-sharing-platform/src/components/newPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function newPage() {
+  return (
+    <div className='bg-blue-300 text-2xl'>
+        <h1>new page</h1>
+        <p>this is a new page</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, aliquam dolores dignissimos iure nesciunt porro doloremque? Quasi id, minus quibusdam natus nesciunt voluptas velit ullam nam dolor, est autem cum?</p>
+    </div>
+  )
+}
+
+export default newPage
