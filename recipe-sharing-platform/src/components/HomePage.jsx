7e484c3fb1962286d66137658119b8754781e797
Allan <nabuleallan1@gmail.com>
2026-02-11 11:39:27 +0300

    added homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ function HomePage() {
           <p>Loading recipes...</p>
         ) : (
           recipes.map(recipe => (
-            <div key={recipe.id} className='container mx-auto p-4 border-2 border-gray-300 rounded-lg mb-4 flex flex-col items-center bg-amber-100 hover:bg-amber-200 transition duration-300 top-1.5 xl:w-1/2 xl:grid-cols-3 lg:w-1/2 lg:grid-cols-3 md:w-full md:grid-cols-1 sm:w-full sm:grid-cols-1'>
+            <div key={recipe.id} className='container mx-auto p-4 border-2 shadow-amber-100 border-gray-300 rounded-lg mb-4 flex flex-col items-center bg-amber-100 hover:bg-amber-200 transition duration-300 top-1.5 xl:w-1/2 xl:grid-cols-3 lg:w-1/2 lg:grid-cols-3 md:w-full md:grid-cols-2 sm:w-full sm:grid-cols-1'>
               <h2 className='font-medium text-neutral-900'>{recipe.title}</h2>
               <p>{recipe.summary}</p>
               <img src={recipe.image} alt={recipe.title} />
