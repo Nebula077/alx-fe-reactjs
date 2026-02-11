ea99959061edd293ff09cb6b0b006b4fadbd67f0
Allan <nabuleallan1@gmail.com>
2026-02-11 11:37:36 +0300

    added homepage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import newPage from './components/newPage'
+import HomePage from './components/HomePage'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
         <h1 className='text-red-500'>Recipe Sharing Platform</h1>
         <p>This is a recipe sharing platform</p>
       </div>
-      <newPage />
+      <HomePage />
     </>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import data from '../data.json'
+
+function HomePage() {
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setRecipes(data)
+    setLoading(false)
+  }, [])
+
+  return (
+    <div>
+      <h3 className='text-blue-500 text-2xl md:text-3xl flex justify-center '>Recipe Sharing Platform</h3>
+        {loading ? (
+          <p>Loading recipes...</p>
+        ) : (
+          recipes.map(recipe => (
+            <div key={recipe.id} className='container mx-auto p-4 border-2 border-gray-300 rounded-lg mb-4 flex flex-col items-center bg-amber-100 hover:bg-amber-200 transition duration-300 top-1.5 xl:w-1/2 xl:grid-cols-3 lg:w-1/2 lg:grid-cols-3 md:w-full md:grid-cols-1 sm:w-full sm:grid-cols-1'>
+              <h2 className='font-medium text-neutral-900'>{recipe.title}</h2>
+              <p>{recipe.summary}</p>
+              <img src={recipe.image} alt={recipe.title} />
+            </div>
+          ))
+        )}
+    </div>
+  )
+}
+
+export default HomePage
